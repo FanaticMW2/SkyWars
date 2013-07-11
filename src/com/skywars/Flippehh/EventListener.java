0359061d3784867e1f6b7b0ee2a477bddbf10ee2
FanaticMW2 <FanaticMW2@email.com>
2013-07-11 14:11:57 -0700

    Updated for all coders to work on
    Nothing much changed, this is just the latest version for the coders

--- src/com/skywars/Flippehh/EventListener.java
+++ src/com/skywars/Flippehh/EventListener.java
@@ -1,21 +1,41 @@
 package com.skywars.Flippehh;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class EventListener implements Listener {
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event){
-		String player = event.getPlayer().getName();
-		event.setJoinMessage(ChatColor.GRAY + player + " has connected.");
+		Player player = event.getPlayer();
+		event.setJoinMessage(ChatColor.GRAY + player.getName() + " has connected.");
+	}
+	@EventHandler
+	public void tabColors(PlayerJoinEvent event){
+		Player player = event.getPlayer();
+		if(player.hasPermission("sky.owner")){
+			player.setPlayerListName(ChatColor.DARK_RED + player.getName());
+		}else if(player.hasPermission("sky.admin")){
+			player.setPlayerListName(ChatColor.RED + player.getName());
+		}else if(player.hasPermission("sky.moderator")){
+			player.setPlayerListName(ChatColor.DARK_PURPLE + player.getName());
+		}
 	}
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event){
-		String player = event.getPlayer().getName();
-		event.setQuitMessage(ChatColor.GRAY + player + " has disconnected.");
+		Player player = event.getPlayer();
+		event.setQuitMessage(ChatColor.GRAY + player.getName() + " has disconnected.");
+	}
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerKicked(PlayerKickEvent event){
+		Player player = event.getPlayer();
+		Bukkit.broadcastMessage(ChatColor.RED + player.getName() + ChatColor.GRAY + " was kicked for: " + event.getReason());
 	}
 }
--- src/com/skywars/Flippehh/Main.java
+++ src/com/skywars/Flippehh/Main.java
@@ -6,10 +6,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	public void onEnable(){
+		loadEssentials();
 		getServer().getLogger().info("SkyWars is enabled.");
+		
+	}
+	
+	void loadEssentials(){
 		PluginManager pm = this.getServer().getPluginManager();
 		pm.registerEvents(new EventListener(), this);
+		//Just for quick copying later :] getCommand("foobar").setExecutor(new CommandHandler());
 	}
+	
 	public void onDisable(){
 		getServer().getLogger().info("SkyWars is enabled.");
 	}
